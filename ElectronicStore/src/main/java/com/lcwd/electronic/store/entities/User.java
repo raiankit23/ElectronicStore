fb4e9b8799269510fc4a2115ef788e2b653ce47f
Ankit Rai <raiankit23@gmail.com>
2023-10-05 02:08:05 +0530

    Create User Entity

---
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/entities/User.java
@@ -0,0 +1,29 @@
+package com.lcwd.electronic.store.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String userId;
+    @Column(name = "user_name")
+    private String name;
+    @Column(name = "user_email", unique = true)
+    private  String email;
+    @Column(name = "user_password", length = 10, nullable = false)
+    private String password;
+    private String gender;
+    @Column(length = 1000)
+    private String about;
+    @Column(name = "user_image_name")
+    private String imageName;
+
+}
